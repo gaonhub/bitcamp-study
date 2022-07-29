6c67a7228b164e9440909482f752d8af2d72eaa6
gaonhub <rkdhs321@gmail.com>
2022-07-29 15:39:19 +0900

    백업

--- java-lang/app/src/main/java/com/eomcs/exception/ex5/Exam0120.java
+++ java-lang/app/src/main/java/com/eomcs/exception/ex5/Exam0120.java
@@ -1,4 +1,4 @@
-// 예외 처리 후
+// 예외 처리 후 - 1) 기존 예외 클래스를 사용하여 예외 객체를 받기
 package com.eomcs.exception.ex5;
 
 import java.sql.Date;
@@ -18,7 +18,15 @@ public class Exam0120 {
   //    예외를 던진다는 것은 이해하지만,
   //    그 예외가 의미하는 바가 무엇인지 즉시 알아보기 힘들다는 얘기다.
   //
-  static Board read() {
+  // read() 메서드에서 발생할 수 있는 예외:
+  //    Integer.paserInt() => NumberFormatException 이 발생할 수 있다.
+  //    Data.valueOf() => IllegalArgumentException 이 발생할 수 있다.
+  // 
+  //  위 두 개의 예외 모두 RuntimeException 계열의 예외이기 때문에
+  //  메서드 선언 후에 throws 문장을 표시할 필요가 없다
+  //  즉 생략해도 된다.
+  //
+  static Board read() /*throws NumberFormatException, IllegalArgumentException */ {
     try (Scanner keyScan = new Scanner(System.in)) {
       Board board = new Board();
 
@@ -48,6 +56,10 @@ public class Exam0120 {
       System.out.printf("등록일: %s\n", board.getCreatedDate());
 
     } catch (RuntimeException e) {
+      // read()에서 발생된 예외를 받을 때는
+      // 두 예외를 모두 받을 수 있도록 두 클래스의 부모인 
+      // RuntimeException 파라미터로 받는다.
+
       System.out.println(e.getMessage());
       System.out.println("게시물 입력 중에 오류 발생!");
       // e.printStackTrace();
