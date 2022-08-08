05c33411eee4a327b698e8c772725b9e7b15965b
gaonhub <rkdhs321@gmail.com>
2022-08-08 11:21:13 +0900

    .

--- board-app/app/src/main/java/com/bitcamp/board/dao/BoardDao.java
+++ board-app/app/src/main/java/com/bitcamp/board/dao/BoardDao.java
@@ -1,8 +1,9 @@
 package com.bitcamp.board.dao;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import com.bitcamp.board.domain.Board;
-import com.bitcamp.util.LinkedList;
-import com.bitcamp.util.List;
 
 // 게시글 목록을 관리하는 역할
 //
@@ -39,14 +40,18 @@ public class BoardDao {
 
   public Board[] findAll() {
 
-    Board[] arr = list.toArray(new Board[0]);
+    // 목록에서 값을 꺼내는 일을 할 객체를 준비한다.
+    Iterator<Board> iterator = list.iterator();
 
     // 역순으로 정렬하여 리턴한다.
-    Board[] arr2 = new Board[arr.length];
-    for (int i = 0; i < arr2.length; i++) {
-      arr2[i] = arr[arr.length - i - 1];
+    Board[] arr = new Board[list.size()];
+
+    int index = list.size() - 1;
+    while (iterator.hasNext()) {
+      arr[index--] = iterator.next();
     }
-    return arr2;
+
+    return arr;
   }
 
   private int nextNo() {
