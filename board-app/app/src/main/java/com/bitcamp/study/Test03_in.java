abb748bed165d30edf8f01a2760f4eb9cbed3462
gaonhub <rkdhs321@gmail.com>
2022-08-09 12:06:49 +0900

    ,

---
+++ board-app/app/src/main/java/com/bitcamp/study/Test03_in.java
@@ -0,0 +1,21 @@
+// FileInputStream: read() 사용법
+package com.bitcamp.study;
+
+import java.io.FileInputStream;
+
+public class Test03_in {
+
+  public static void main(String[] args) throws Exception {
+    FileInputStream in = new FileInputStream("test3.data");
+
+    int b;
+    while ((b = in.read()) != -1) {
+      System.out.printf("%08x\n", b);
+    }
+
+    in.close();
+
+    System.out.println("실행 완료!");
+  }
+
+}
