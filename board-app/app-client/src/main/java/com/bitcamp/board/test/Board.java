1b5e4dd19346ba70f1005cc311e2f3f9a2d1dc74
gaonhub <rkdhs321@gmail.com>
2022-09-07 20:53:06 +0900

    .

---
+++ board-app/app-client/src/main/java/com/bitcamp/board/test/Board.java
@@ -0,0 +1,49 @@
+package com.bitcamp.board.test;
+
+import java.sql.Date;
+
+public class Board {
+  public int no;
+  public String title;
+  public String content;
+  public Date registeredDate;
+  public int viewCount;
+
+  @Override
+  public String toString() {
+    return "Board [no=" + no + ", title=" + title + ", content=" + content + ", registeredDate="
+        + registeredDate + ", viewCount=" + viewCount + "]";
+  }
+  public int getNo() {
+    return no;
+  }
+  public void setNo(int no) {
+    this.no = no;
+  }
+  public String getTitle() {
+    return title;
+  }
+  public void setTitle(String title) {
+    this.title = title;
+  }
+  public String getContent() {
+    return content;
+  }
+  public void setContent(String content) {
+    this.content = content;
+  }
+  public Date getRegisteredDate() {
+    return registeredDate;
+  }
+  public void setRegisteredDate(Date registeredDate) {
+    this.registeredDate = registeredDate;
+  }
+  public int getViewCount() {
+    return viewCount;
+  }
+  public void setViewCount(int viewCount) {
+    this.viewCount = viewCount;
+  }
+
+
+}
--- java-lang/app/src/main/java/com/eomcs/jdbc/ex2/Exam0210.java
+++ java-lang/app/src/main/java/com/eomcs/jdbc/ex2/Exam0210.java
@@ -1,4 +1,12 @@
 // 게시판 관리 - JDBC 코드를 별도의 클래스로 캡슐화시킴. DAO 적용.
+// 1) Board 객체를 만든다.
+// 2) 사용자에게 입력받을 Scanner를 준비한다.
+// 3) 이 클래스에서 try블럭 두 개를 사용한다.
+// 3) 제목 내용을 입력받는다.
+// 4) 입력 확인을 받는다.
+// 5) BoardDao를 생성한다.
+// 6) 입력 성공을 출력한다.
+// 7) catch에서 프린트스택트레이스를 사용한다.
 package com.eomcs.jdbc.ex2;
 
 import java.util.Scanner;
@@ -6,23 +14,20 @@ import java.util.Scanner;
 public class Exam0210 {
 
   public static void main(String[] args) throws Exception {
-
     Board board = new Board();
 
-    try (Scanner keyScan = new Scanner(System.in)) {
+    try(Scanner keyscan = new Scanner(System.in)) {
+      System.out.print("제목?");
+      board.setTitle(keyscan.nextLine());
 
-      // 사용자로부터 제목, 내용을 입력 받는다.
-      System.out.print("제목? ");
-      board.setTitle(keyScan.nextLine());
+      System.out.print("내용?");
+      board.setContent(keyscan.nextLine());
 
-      System.out.print("내용? ");
-      board.setContent(keyScan.nextLine());
+      System.out.print("입력하시겠습니까?(y/n)");
+      String input = keyscan.nextLine();
 
-      System.out.print("입력하시겠습니까?(Y/n) ");
-      String input = keyScan.nextLine();
-
-      if (!input.equalsIgnoreCase("y") && input.length() != 0) {
-        System.out.println("등록을 취소 하였습니다.");
+      if(!input.equalsIgnoreCase("y") && input.length() != 0) {
+        System.out.println("등록을 취소하였습니다.");
         return;
       }
     }
@@ -30,29 +35,7 @@ public class Exam0210 {
     try {
       BoardDao boardDao = new BoardDao();
       int count = boardDao.insert(board);
-      System.out.printf("%d 개 입력 성공!", count);
-
-      // 이렇게 DBMS 작업을 별도의 클래스로 분리하여 캡슐화하면
-      // 데이터 처리를 요구하는 쪽에서는
-      // 데이터의 구체적인 처리 방식을 몰라도 되기 때문에
-      // 코드가 간결해진다.
-      // 또한 데이터 처리 방식이 바뀌더라도 영향을 받지 않는다.
-      //
-      // [데이터처리를 사용하는 측]......[데이터를 처리하는 측(DAO)]
-      // .........|.....................................|
-      // .........|..생성...............................|
-      // .........|------> [도메인 객체]................|
-      // .........|.....................................|
-      // .........|........메서드 호출..................|
-      // .........|------------------------------------>|
-      // .........|.....................................|
-      //
-      // 도메인 객체(예: Board)
-      // => 두 객체 사이에 데이터를 실어 나르는 역할을 한다고 해서
-      //    "DTO(Data Transfer Object)"라고도 부른다.
-      // => 두 객체 사이에 전달되는 값을 표현한다고 해서
-      //    "VO(Value Object)"라고도 부른다.
-      //
+      System.out.printf("%s개 입력 성공!", count);
     } catch (Exception e) {
       e.printStackTrace();
     }
