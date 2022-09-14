c64037f948cc1dbb011a3e6bd55a3addc1e7cb05
gaonhub <rkdhs321@gmail.com>
2022-09-14 15:38:56 +0900

    .

--- board-app/app-server/src/main/java/com/bitcamp/util/Response.java
+++ board-app/app-server/src/main/java/com/bitcamp/util/Response.java
@@ -3,7 +3,8 @@ package com.bitcamp.util;
 import java.io.DataOutputStream;
 
 public class Response {
-  public void sendMessage(DataOutputStream out) throws Exception {
+
+  public void sendMessage(DataOutputStream out, String message) throws Exception {
 
   }
 }
