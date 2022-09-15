ca7b97078cfb24cb4b57d2a3c74d3e4b6e543b24
gaonhub <rkdhs321@gmail.com>
2022-09-15 16:07:07 +0900

    .

--- board-app/app-server/src/main/java/com/bitcamp/board/MiniWebServer.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/MiniWebServer.java
@@ -6,6 +6,16 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.net.InetSocketAddress;
 import java.net.URI;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.HashMap;
+import java.util.Map;
+import com.bitcamp.board.dao.BoardDao;
+import com.bitcamp.board.dao.MariaDBBoardDao;
+import com.bitcamp.board.dao.MariaDBMemberDao;
+import com.bitcamp.board.dao.MemberDao;
+import com.bitcamp.board.handler.BoardHandler;
+import com.bitcamp.board.handler.ErrorHandler;
 import com.bitcamp.board.handler.WelcomeHandler;
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
@@ -15,6 +25,15 @@ import com.sun.net.httpserver.HttpServer;
 public class MiniWebServer {
 
   public static void main(String[] args) throws Exception {
+    Connection con = DriverManager.getConnection(
+        "jdbc:mariadb://localhost:3306/studydb","study","1111");
+
+    BoardDao boardDao = new MariaDBBoardDao(con);
+    MemberDao memberDao = new MariaDBMemberDao(con);
+
+    WelcomeHandler welcomeHandler = new WelcomeHandler();
+    ErrorHandler errorHandler = new ErrorHandler();
+    BoardHandler boardHandler = new BoardHandler(boardDao);
 
     class MyHttpHandler implements HttpHandler {
       @Override
@@ -22,20 +41,43 @@ public class MiniWebServer {
         System.out.println("클라이언트가 요청함!");
 
         URI requestUri = exchange.getRequestURI();
-        System.out.println(requestUri.getScheme());
-        System.out.println(requestUri.getHost());
-        System.out.println(requestUri.getPort());
-        System.out.println(requestUri.getPath());
 
-        WelcomeHandler welcomeHandler = new WelcomeHandler();
+        String path = requestUri.getPath();
+        String query = requestUri.getQuery();
+
+        Map<String,String> paramMap = new HashMap<>();
+        if (query != null && query.length() > 0) { // 예) no=1&title=aaaa&content=bbb
+          String[] entries = query.split("&");
+          for (String entry : entries) { // 예) no=1
+            String[] kv = entry.split("=");
+            paramMap.put(kv[0], kv[1]);
+          }
+        }
+
+        System.out.println(paramMap);
 
         byte[] bytes = null;
 
         try (StringWriter stringWriter = new StringWriter();
             PrintWriter printWriter = new PrintWriter(stringWriter)) {
 
-          welcomeHandler.service(printWriter);
+          if (path.equals("/")) {
+            welcomeHandler.service(paramMap, printWriter);
+
+          } else if (path.equals("/board/list")) {
+            boardHandler.list(paramMap, printWriter);
+
+          } else if (path.equals("/board/detail")) { 
+            boardHandler.detail(paramMap, printWriter);
+
+          } else {
+            errorHandler.error(paramMap, printWriter);
+          }
           bytes = stringWriter.toString().getBytes("UTF-8");
+
+        } catch (Exception e) {
+          bytes = "요청 처리 중 오류 발생!".getBytes("UTF-8");
+          e.printStackTrace(); // 서버 콘솔 창에 오류에 대한 자세한 내용을 출력한다.
         }
 
         // 보내는 콘텐트의 MIME 타입이 무엇인지 응답 헤더에 추가한다.
--- board-app/app-server/src/main/java/com/bitcamp/board/handler/ErrorHandler.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/handler/ErrorHandler.java
@@ -3,8 +3,8 @@ package com.bitcamp.board.handler;
 import java.io.PrintWriter;
 import java.util.Map;
 
-public class WelcomeHandler {
-  public void service(Map<String,String> paramMap, PrintWriter out) {
+public class ErrorHandler {
+  public void error(Map<String,String> paramMap, PrintWriter out) {
     out.println("<!DOCTYPE html>");
     out.println("<html>");
     out.println("<head>");
@@ -12,8 +12,8 @@ public class WelcomeHandler {
     out.println("<title>bitcamp</title>");
     out.println("</head>");
     out.println("<body>");
-    out.println("<h1>환영합니다!2</h1>");
-    out.println("<p>비트캠프 게시판 관리 시스템 프로젝트입니다.</p>");
+    out.println("<h1>요청 오류!</h1>");
+    out.println("<p>요청한 자원을 찾을 수 없습니다.</p>");
     out.println("</body>");
     out.println("</html>");
   }
