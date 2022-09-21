6ac2cdca105c36fd7d851a75c1eee21354ae865f
gaonhub <rkdhs321@gmail.com>
2022-09-21 17:18:59 +0900

    .

--- board-app/app-server/src/main/java/com/bitcamp/board/servlet/AppInitServlet.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/servlet/AppInitServlet.java
@@ -1,57 +1,42 @@
 package com.bitcamp.board.servlet;
 
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import com.bitcamp.board.dao.BoardDao;
 import com.bitcamp.board.dao.MariaDBBoardDao;
 import com.bitcamp.board.dao.MariaDBMemberDao;
-import com.bitcamp.board.dao.MemberDao;
 
 // 이 서블릿은 다른 서블릿이 사용할 객체를 준비하는 일을 한다.
 
-@WebServlet(value="/init")
+@WebServlet(
+    value="/init", 
+    loadOnStartup=1)
 public class AppInitServlet extends HttpServlet {
 
   private static final long serialVersionUID = 1L;
 
-  public static BoardDao boardDao;
-  public static MemberDao memberDao;
-
-  public AppInitServlet() throws Exception {
-    System.out.println("공유 자원을 준비 중!");
-
-    Class.forName("org.mariadb.jdbc.Driver");
-
-    Connection con = DriverManager.getConnection(
-        "jdbc:mariadb://localhost:3306/studydb","study","1111");
-    boardDao = new MariaDBBoardDao(con);
-    memberDao = new MariaDBMemberDao(con);
-  }
-
   @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse res)
-      throws ServletException, IOException {
+  public void init() throws ServletException {
+    // 톰캣서버 --> init(ServletConfig) --> init()
+    System.out.println("공유 자원을 준비 중!");
+    try {
+      Class.forName("org.mariadb.jdbc.Driver");
+      Connection con = DriverManager.getConnection(
+          "jdbc:mariadb://localhost:3306/studydb","study","1111");
 
-    res.setContentType("text/html; charset=UTF-8");
-    PrintWriter out = res.getWriter();
-
-    out.println("<!DOCTYPE html>");
-    out.println("<html>");
-    out.println("<head>");
-    out.println("<meta charset=\"UTF-8\">");
-    out.println("<title>bitcamp</title>");
-    out.println("</head>");
-    out.println("<body>");
-    out.println("<h1>웹 애플리케이션 자원 준비!</h1>");
-    out.println("</body>");
-    out.println("</html>");
+      // 생성자에서 getServletContext()를 호출하면 오류 발생!
+      // 왜? 아직 ServletContext 객체가 준비되지 않았기 때문이다.
+      ServletContext ctx = this.getServletContext();
+      ctx.setAttribute("boardDao", new MariaDBBoardDao(con));
+      ctx.setAttribute("memberDao", new MariaDBMemberDao(con));
+    } catch (Exception e) {
+      throw new ServletException();
+    }
   }
 
+  // 이 서블릿의 역할은 다른 서블릿이 사용할 자원을 준비하는 것이기 때문에
+  // 굳이 요청을 처리하는 메서드를 정의할 필요가 없다.
 }
