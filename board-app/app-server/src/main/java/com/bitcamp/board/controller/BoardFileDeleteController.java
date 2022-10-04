93b18586343052eae411e2b145b2d05a86172784
gaonhub <rkdhs321@gmail.com>
2022-10-04 12:33:42 +0900

    .

--- board-app/app-server/src/main/java/com/bitcamp/board/controller/BoardFileDeleteController.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/controller/BoardFileDeleteController.java
@@ -7,9 +7,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.bitcamp.board.dao.BoardDao;
+import com.bitcamp.board.domain.AttachedFile;
+import com.bitcamp.board.domain.Board;
 import com.bitcamp.board.domain.Member;
 
-@WebServlet("/board/delete")
+@WebServlet("/board/fileDelete")
 public class BoardFileDeleteController extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
@@ -26,29 +28,23 @@ public class BoardFileDeleteController extends HttpServlet {
     try {
       int no = Integer.parseInt(request.getParameter("no"));
 
+      // 첨부파일 정보를 가져온다.
+      AttachedFile attachedFile = boardDao.findFileByNo(no);
+
+      // 게시글의 작성자가 로그인 사용자인지 검사한다.
       Member loginMember = (Member) request.getSession().getAttribute("loginMember");
-      if (boardDao.findByNo(no).getWriter().getNo() != loginMember.getNo()) {
+      Board board = boardDao.findByNo(attachedFile.getBoardNo());
+
+      if (board.getWriter().getNo() != loginMember.getNo()) {
         throw new Exception("게시글 작성자가 아닙니다.");
       }
 
-      if (boardDao.delete(no) == 0) {
-        throw new Exception("게시글 삭제 실패!");
+      // 첨부파일을 삭제한다.
+      if (boardDao.deleteFile(no) == 0) {
+        throw new Exception("게시글 첨부파일 삭제 실패!");
       }
 
-      // Redirect:
-      // - 클라이언트에게 콘텐트를 보내지 않는다.
-      // - 응답 프로토콜
-      //      HTTP/1.1 302   <=== 응답 상태 코드
-      //      Location: list  <=== 자동으로 요청할 URL
-      //      Content-Length: 0  <=== 콘텐트는 보내지 않는다.
-      //      Date: Mon, 26 Sep 2022 05:21:22 GMT
-      //      Keep-Alive: timeout=20
-      //      Connection: keep-alive
-      // 
-      //      (콘텐트 없음!)
-      //
-      // 자바 코드:
-      response.sendRedirect("list");
+      response.sendRedirect("detail?no=" + board.getNo());
 
     } catch (Exception e) {
       request.setAttribute("exception", e);
