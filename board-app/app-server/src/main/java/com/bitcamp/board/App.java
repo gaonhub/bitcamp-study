ae72b383596a17742200691dcf7de573888beb5c
gaonhub <rkdhs321@gmail.com>
2022-10-20 13:02:26 +0900

    .

--- board-app/app-server/src/main/java/com/bitcamp/board/App.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/App.java
@@ -2,10 +2,12 @@ package com.bitcamp.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 @EnableTransactionManagement
-//@MapperScan("com.bitcamp.board.dao") 
 @SpringBootApplication
 public class App {
 
@@ -13,6 +15,11 @@ public class App {
     System.out.println("비트캠프 프로젝트!");
 
     SpringApplication.run(App.class, args);
+  }
+
+  @GetMapping("/")
+  public String welcome() {
+    return "welcome";
   }
 
 }
